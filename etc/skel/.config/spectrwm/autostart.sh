08c6a4816293a50288b2ced0486d2800e7f906a4
Kungger-git <matthewriel63@gmail.com>
2021-12-11 15:10:19 +0800

    Update autostart script

--- etc/skel/.config/spectrwm/autostart.sh
+++ etc/skel/.config/spectrwm/autostart.sh
@@ -31,3 +31,7 @@ udiskie &
 
 # replace neovim colorscheme
 sed -i "s/theme =.*$/theme = \"everforest\",/g" $HOME/.config/nvim/lua/chadrc.lua
+
+# change xfce4-terminal colorscheme
+XFCE_TERM_PATH="$HOME/.config/xfce4/terminal"
+cp "$XFCE_TERM_PATH"/colorschemes/everforest "$XFCE_TERM_PATH"/terminalrc
